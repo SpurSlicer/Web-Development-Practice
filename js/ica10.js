b4ee99b81c495b4707d73e7f7ac4900d9c1f3a0f
SpurSlicer <spur27@proton.me>
2024-03-19 10:41:34 -0600

    fixed ica10

--- js/ica10.js
+++ js/ica10.js
@@ -1,20 +1,22 @@
-const text = "hello!";
 const arr = ["Heyy!", "I am text", "arr[2]", "js", "only runs on", "1 thread :("];
 const colorArr = ["lightcoral", "lightsalmon", "lightgoldenrodyellow", "lightgreen", "lightskyblue", "plum"];
-let cnt = 0;
+let cnt = 0, cnt2 = 0;
 const h1 = document.querySelector("h1");
-const button = document.querySelector("button");
+const buttonTexter = document.querySelector("#texter");
+const buttonStyler = document.querySelector("#styler");
 const html = document.querySelector("html");
 const h3 = document.querySelector("h3");
 const title = document.querySelector("title");
-button.addEventListener("click", modify);
-function modify() {
+buttonTexter.addEventListener("click", modify1);
+buttonStyler.addEventListener("click", modify2);
+function modify1() {
     h1.textContent = `Thank you for clicking! Here is a random number ${(Math.random()*10).toFixed(0)}`;
-    button.textContent = arr[cnt%6];
+    buttonTexter.textContent = arr[cnt%6];
     title.textContent = `The button says ${arr[cnt%6]}`;
-    html.style.backgroundColor = colorArr[cnt%6];
     cnt++;
-    button.style.width += 10;
-    h3.textContent = `You've clicked ${cnt} times`;
-    console.log("I got here");
+    h3.textContent = `You've clicked text button ${cnt} times`;
+}
+function modify2() {
+    html.style.backgroundColor = colorArr[cnt2%6];
+    cnt2++;
 }
